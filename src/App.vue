7d65d915ecb1c9ccac0875cc8c3e132bb2b41001
mattemutti <mattemutti@gmail.com>
2024-04-19 09:01:16 +0200

    initial site

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+
+  name: 'App', 							// nome del componente
+
+  //qui ci va il codice di vue
+  data() {
+    return {
+      message: 'By'
+    }
+  },
+  methods: {
+    // qui le funzioni
+  },
+  mounted() {
+    // qui i log nell hook mounted
+  }
+}
+</script>
+
+<template>
+
+
+
+
+
+  <!-- // qui scriveremo il codice html -->
+  <h1>{{ message }}</h1>
+</template>
+
+<style>
+//qui scriveremo lo stile del foglio css
+* {
+  border: 1px solid black;
+  background-color: red;
+}
+</style>
